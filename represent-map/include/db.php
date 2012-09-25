496bcab3d888d814fd39d43a488e09dc618d36d9
lazylad007 <lazylad007@gmail.com>
2012-09-25 21:45:14 +0530

    final

--- represent-map/include/db.php
+++ represent-map/include/db.php
@@ -21,7 +21,7 @@ $admin_pass = "admin";
 // HTML that goes just before </head>
 $head_html = "";
 
-// attribution (must leave link intact, per our license) 
+// attribution (must leave link intact, per our license)
 $attribution = "
   <span>
     Based on <a href='http://www.represent.la' target='_blank'>RepresentLA</a>
